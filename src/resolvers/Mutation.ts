e3da0c1c4a99df76b17cbd11e51eb6bc7d141af0
AbuMusaa <musahamim.bd@gmail.com>
2022-04-10 15:12:33 +0600

    Defining GraphQL Schema

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,1 +1,22 @@
-export const Mutation = {};
+import { Context } from "../index";
+
+interface createPostArgs {
+  title: string;
+  content: string;
+}
+
+export const Mutation = {
+  createPost: async (
+    _: any,
+    { title, content }: createPostArgs,
+    { prisma }: Context
+  ) => {
+    const post = await prisma.post.create({
+      data: {
+        title,
+        content,
+        authorId: 1,
+      },
+    });
+  },
+};
--- src/schema.ts
+++ src/schema.ts
@@ -4,4 +4,40 @@ export const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  type Mutation {
+    createPost(title: String!, content: String!): PostPayload!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String!
+    createdAt: String!
+    published: Boolean!
+    user: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    posts: [Post]
+    profile: Profile
+  }
+
+  type Profile {
+    id: ID!
+    bio: String!
+    user: User!
+  }
+
+  type UserError {
+    massage: String!
+  }
+
+  type PostPayload {
+    userError: [UserError!]!
+    post: Post
+  }
 `;
